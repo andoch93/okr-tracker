665d2a8edf9f87a127440f3aa85fc1666842b2e4
Andres Ochoa <andoch93@gmail.com>
2021-03-19 16:55:58 -0500

    Added basic foundation for Feature section

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import { Section } from '@components/Layout/Section';
+import classes from '@components/Feature/feature.css';
 
 export function Feature(): React.ReactElement {
   return (
     <Section>
-      <h3>Feature</h3>
+      <Row className="justify-content-md-center">
+        <Col xs lg="5">
+          <h3 className="h4">Feature</h3>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, earum! Totam ut rerum beatae necessitatibus in
+            mollitia quos voluptates iste? Rem sequi eius accusantium, excepturi harum ipsam voluptas atque cupiditate!
+          </p>
+        </Col>
+        <Col xs lg="5" className="text-center">
+          <div className={classes.box}></div>
+        </Col>
+      </Row>
     </Section>
   );
 }
